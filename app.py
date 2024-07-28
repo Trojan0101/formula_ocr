d00cf66aa4fdf2aad29c2e98972777261407cbb1
Trojan0101 <aloysius.i.05@protonmail.com>
2024-07-29 01:51:28 +0530

    test_deployement_1

--- app.py
+++ app.py
@@ -12,33 +12,22 @@ from data_extractors.text_extractor import TextExtractor
 app = Flask(__name__)
 
 
-@app.route('/convert_latex', methods=['POST'])
-def convert_latex():
-    request_data = request.get_json()
-    image_url = request_data.get("src")
-
-    try:
-        latex_extractor = LatexExtractor()
-        response_data = latex_extractor.convert_image_to_latex(url=image_url)
-    except Exception as e:
-        response_data = {"error": str(e)}
-
-    return jsonify(response_data)
-
-
 @app.route('/convert_text', methods=['POST'])
 def convert_text():
     request_data = request.get_json()
     image_url = request_data.get("src")
-    image_language = [x.strip() for x in request_data.get("language").split(",")]
 
     try:
-        text_extractor = TextExtractor(image_language)
-        response_data = text_extractor.convert_image_to_text(url=image_url)
-    except Exception as e:
-        response_data = {"error": str(e)}
+        text_extractor = TextExtractor()
+        response_data_text = text_extractor.convert_image_to_text(url=image_url)
 
-    return jsonify(response_data)
+        latex_extractor = LatexExtractor()
+        response_data_latex = latex_extractor.convert_image_to_latex(url=image_url)
+        final_response = {"text": response_data_text["text"], "latex": response_data_latex["latex_text"]}
+    except Exception as e:
+        final_response = {"error": str(e)}
+
+    return jsonify(final_response)
 
 
 if __name__ == '__main__':
--- data_extractors/text_extractor.py
+++ data_extractors/text_extractor.py
@@ -4,12 +4,13 @@ Author: Trojan
 Date: 20-07-2024
 """
 import os
+from io import StringIO
 from urllib.parse import urlparse
 
 import cv2
-import matplotlib.pyplot as plt
-import easyocr
-from typing import List
+import pandas as pd
+import numpy as np
+import pytesseract
 
 import requests
 
@@ -18,33 +19,79 @@ from data_extractors.utils.response_object_text import ResponseObjectText
 
 class TextExtractor:
 
-    def __init__(self, language_combination: List[str]):
-        self.reader = easyocr.Reader(language_combination)
+    def __init__(self):
         self.downloaded_file_path = os.path.join("downloaded_images", "verification_image.png")
 
     def convert_image_to_text(self, url: str) -> dict:
         response_object = None
 
+        def preprocess_image(downloaded_image_path):
+            img = cv2.imread(downloaded_image_path)
+
+            # Ensure minimum DPI (300 DPI guideline)
+            dpi = 300
+            width_inch = img.shape[1] / dpi
+            height_inch = img.shape[0] / dpi
+            
+            if width_inch < 1 or height_inch < 1:
+                # If image is smaller than 300 DPI, resize to meet minimum DPI requirement
+                new_width = int(width_inch * dpi)
+                new_height = int(height_inch * dpi)
+                img_resized = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
+            else:
+                img_resized = img
+            
+            # Convert to grayscale
+            img_gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)
+            
+            # # Apply adaptive thresholding to capture varying text colors
+            img_binary = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
+            # # Define the sharpening kernel
+            kernel = np.array([[-1, -1, -1],
+                        [-1,  9, -1],
+                        [-1, -1, -1]])
+            sharpened_image = cv2.filter2D(img_binary, -1, kernel)
+        
+            # Plot the image using Matplotlib
+            # plt.figure(figsize=(10, 10))
+            # plt.imshow(sharpened_image, cmap='gray')
+            # plt.axis('off')
+            # plt.show()
+            
+            return sharpened_image
+
         def process_image(downloaded_image_path):
             try:
-                img = cv2.imread(downloaded_image_path)
-                result = self.reader.readtext(downloaded_image_path)
-                # Extract the text and draw bounding boxes
-                extracted_text = []
-                for detection in result:
-                    top_left = (int(detection[0][0][0]), int(detection[0][0][1]))
-                    bottom_right = (int(detection[0][2][0]), int(detection[0][2][1]))
-                    text = detection[1]
-                    img = cv2.rectangle(img, top_left, bottom_right, (0, 255, 0), 3)
-                    img = cv2.putText(img, text, (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
-                    extracted_text.append(text)
+                img_preprocessed = preprocess_image(downloaded_image_path)
+                languages = [['eng'], ['eng', 'jpn'], ['eng', 'chi_sim', 'chi_tra'], ['eng', 'kor']]
+                text_confidence = {}
 
-                # Display the output
-                plt.figure(figsize=(10, 10))
-                plt.imshow(img)
-                plt.show()
+                for language in languages:
+                    # Initialize configuration parameters for Tesseract OCR
+                    config_eng = f'--oem 3 --psm 3 -l {"+".join(language)}'
 
-                return extracted_text
+                    # Use pytesseract to do OCR on the preprocessed image
+                    ocr_data_str = pytesseract.image_to_data(img_preprocessed, config=config_eng)
+                    ocr_data_io = StringIO(ocr_data_str)
+                    ocr_data = pd.read_csv(ocr_data_io, delimiter='\t')
+
+                    extracted_text = ' '.join(ocr_data['text'].dropna().tolist())
+                    confidence_scores = ocr_data['conf'].dropna().astype(float)
+                    # Calculate average confidence score
+                    average_confidence = confidence_scores.mean() if not confidence_scores.empty else None
+                    text_confidence[f'Language: {"+".join(language)}'] = {
+                        'text': extracted_text,
+                        'average_confidence': average_confidence
+                    }
+                if text_confidence:
+                    best_lang = max(text_confidence,
+                                    key=lambda k: text_confidence[k]['average_confidence']
+                                    if text_confidence[k]['average_confidence'] is not None else -1)
+                    best_text = text_confidence[best_lang]['text']
+                    best_confidence = text_confidence[best_lang]['average_confidence']
+
+                    return best_text
+
             except Exception as exc:
                 return {"error": str(exc)}
 
