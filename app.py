0a96f2ca2b432fe4adebcd7628ff6602b76bbd66
Trojan0101 <aloysius.i.05@protonmail.com>
2024-07-30 23:16:38 +0530

    test_deployement_1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Date: 25-06-2024
 """
 
 from flask import Flask, request, jsonify
-
+# from waitress import serve
 from data_extractors.latex_extractor import LatexExtractor
 from data_extractors.text_extractor import TextExtractor
 
@@ -32,3 +32,4 @@ def convert_text():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    # serve(app, host='0.0.0.0', port=8080)
